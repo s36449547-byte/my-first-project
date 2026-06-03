466d903c83fce07706c5753acdfb1f597c7b3e9a
s36449547-byte <s36449547@gmail.com>
2026-06-03 20:00:58 +0500

    Update code1.py

--- code1.py
+++ code1.py
@@ -3,3 +3,18 @@
 3. print(" ||||")
 4. print('*' * 10)
 5. print("♥️")
+6. price = 10
+7. rating = 4.8
+8. name = 'Tom'
+9. is_published = False
+10. print(is_published)
+11. full_name = 'Liam Smith's
+12. age = 26
+13. is_new = True
+14. name = input('What is your name? ')
+15. print('Hi ' + name)
+16. color = input('What is your favorite color? ')
+17. print(name + ' likes ' + color)
+18. birth_year = input('What is your birth year? ')
+19. age = 2026 - int(birth_year)
+20. print(age)
