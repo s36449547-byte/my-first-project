bbb2a6dce7268bfa9a33fba2820f8129209bc7fa
s36449547-byte <s36449547@gmail.com>
2026-06-04 17:44:51 +0500

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,20 +1,20 @@
-1. print("Canada")
-2. print("o----")
-3. print(" ||||")
-4. print('*' * 10)
-5. print("♥️")
-6. price = 10
-7. rating = 4.8
-8. name = 'Tom'
-9. is_published = False
-10. print(is_published)
-11. full_name = 'Liam Smith'
-12. age = 26
-13. is_new = True
-14. name = input('What is your name? ')
-15. print('Hi ' + name)
-16. color = input('What is your favorite color? ')
-17. print(name + ' likes ' + color)
-18. birth_year = input('What is your birth year? ')
-19. age = 2026 - int(birth_year)
-20. print(age)
+print("Canada")
+print("o----")
+print(" ||||")
+print('*' * 10)
+print("♥️")
+price = 10
+rating = 4.8
+name = 'Tom'
+is_published = False
+print(is_published)
+full_name = 'Liam Smith'
+age = 26
+is_new = True
+name = input('What is your name? ')
+print('Hi ' + name)
+color = input('What is your favorite color? ')
+print(name + ' likes ' + color)
+birth_year = input('What is your birth year? ')
+age = 2026 - int(birth_year)
+print(age)
